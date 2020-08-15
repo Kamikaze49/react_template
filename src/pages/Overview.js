90a164138d394fdacd61e43b9c7173925880ae09
Edwin Kayang <pelpuo2000@gmail.com>
2020-08-15 21:32:23 +0000

    Added overview page

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -4,6 +4,8 @@ import {withSideBar} from '../components/SideBar';
 import UserContext from "./../contexts/UserContext";
 import {auth} from "./../firebase";
 
+import OrderCard from "./../components/OrderCard"
+
 function Overview() {
     const [user, setUser] = useContext(UserContext)
     const history = useHistory() 
@@ -17,10 +19,58 @@ function Overview() {
 
     return (
         <div className="Page Overview">
-             <div className="Header">
+            
+            <div className="Header">
                 <h2>Overview</h2>
                 <button onClick={()=>logOut()}>Log Out</button>
             </div>
+
+            <div className="RecentOrders">
+                 <div className="Header">
+                    <h2>Recent Orders</h2>
+                 </div>
+                 <div className="RecentOrdersBody">
+                    <OrderCard
+                        name="John Doe"
+                        id="4gfdth354"
+                        arrived="Today"
+                        completed={false}
+                        patient_id="123461"
+                        status="Pending"
+                    />
+                    <OrderCard
+                        name="John Doe"
+                        id="4gfdth354"
+                        arrived="Today"
+                        completed={false}
+                        patient_id="123461"
+                        status="Pending"
+                    />
+                    <OrderCard
+                        name="John Doe"
+                        id="4gfdth354"
+                        arrived="Today"
+                        completed={false}
+                        patient_id="123461"
+                        status="Pending"
+                    />
+                 </div>
+            </div>
+
+            <div className="info">
+                <div className="Header">
+                    <h2>Stats</h2>
+                </div>
+                <div className="StatsBody">
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                 </div>
+            </div>
+
         </div>
     )
 }
