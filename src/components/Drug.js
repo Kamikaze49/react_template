5fc3a9e2891bff80f0b7f8078c957cbb2d4bfff4
Edwin Kayang <pelpuo2000@gmail.com>
2020-09-13 18:49:35 +0000

    Editted drug component

--- src/components/Drug.js
+++ src/components/Drug.js
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-function Drug() {
+function Drug({status, brand, price, quantity}) {
+    const [drugBrand, setDrugBrand] = useState(null)
+    const [drugQuantity, setDrugQuantity] = useState(null)
+    const [drugPrice, setDrugPrice] = useState(null)
+    const [drugPrescription, setDrugPrescription] = useState(null)
+
+
     return (
         <div className="drugCard">
             <div className="cardHeader">
                 <h4>Paracetamol - 500mg</h4>
             </div>
             <div className="cardContent">
-                <input type="text" placeholder="Brand"/>
+                <input disabled={status==="accepted"} type="text" 
+                placeholder="Brand" 
+                value={drugBrand}
+                onChange={(e)=> setDrugBrand(e.target.value)}/>
                 <div>
-                <select>
-                    <option>Quantity per...</option>
-                    <option>Box</option>
-                    <option>Strip</option>
-                    <option>Bottle</option>
-                    <option>Can</option>
+                <select disabled={status==="accepted"} value={drugQuantity} onChange={(e)=> setDrugQuantity(e.target.value)} >
+                    <option value="">Quantity per...</option>
+                    <option value="Box">Box</option>
+                    <option value="Strip">Strip</option>
+                    <option value="Bottle">Bottle</option>
+                    <option value="Tablet">Tablet</option>
                 </select>
-                <input type="number" placeholder="Price(GHC)"/>
+                <input type="number" 
+                disabled={status==="accepted"}
+                placeholder="Price(GHC)" 
+                value={drugPrice} 
+                onChange={(e)=> setDrugPrice(e.target.value)}/>
                 </div>
+
+                {status === "accepted" &&
+                <div className="Prescription">
+                <h3>
+                    Prescription Details
+                </h3>
+                <textarea rows="4" value={drugPrescription} onChange={e=> setDrugPrescription(e.target.value)}/>
+            </div>}
             </div>
 
         </div>
